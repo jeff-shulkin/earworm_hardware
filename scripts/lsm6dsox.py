2dd9f9e22179cfe1a68208161b7cd92a8dc07e97
ruiying615 <ruiyingy@umich.edu>
2025-02-10 15:26:38 -0500

    add LIS2DW12 and LSM6DSOX(not tested)

--- scripts/lsm6dsox.py
+++ scripts/lsm6dsox.py
@@ -6,4 +6,12 @@ import board
 from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
 
 def init():
-    
+    i2c = board.I2C()
+    sensor = LSM6DSOX(i2c)
+
+    # Continuously printing out
+    while True:
+        print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (sensor.acceleration))
+        print("Gyro X:%.2f, Y: %.2f, Z: %.2f radians/s" % (sensor.gyro))
+        print("")
+        time.sleep(0.5)
